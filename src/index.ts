2d5f83e896f3153775733e7d4a46142f0ff3bfe2
Qtrixnet <qtrixnet@yandex.ru>
2022-10-07 10:14:32 +0600

    feat: add register

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Block, renderDOM, registerComponent }  from './core';
 import LoginPage from './components/login';
+import RegisterPage from "./components/register";
+import './styles/index.css';
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderDOM(new LoginPage({}));
+  renderDOM(new RegisterPage({}));
 });
