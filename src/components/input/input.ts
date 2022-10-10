5cf7027dfad9a8e553faf73a16996f89292ec5e9
Kirill <qtrixnet@yandex.ru>
2022-10-10 22:37:30 +0600

    feat: add login inputs

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,9 +1,17 @@
 import Block from '../../core/Block';
 import './input.css';
 
+interface InputProps {
+  onChange?: () => void;
+  type?: 'text' | 'password' | 'email';
+  placeholder?: string;
+  value?: string;
+  error?: string;
+}
+
 export class Input extends Block {
-  constructor() {
-    super();
+  constructor({onChange = () => {}, type = 'text', error, placeholder, value}: InputProps) {
+    super({type, placeholder, value, error, events: {input: onChange}});
   }
 
   render() {
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -13,9 +13,8 @@ export class Login extends Block {
             <form class="login__form">
                 <h2 class="login__title">Вход</h2>
                 <fieldset class="login__fieldset">
-                    {{#each login}}
-                        {{> input/input}}
-                    {{/each}}
+                    {{{Input type="text" name="login" placeholder="Логин" label="Логин:" value="login"}}}
+                    {{{Input type="password" name="password" placeholder="Пароль" label="Пароль:" value="123456578"}}}
                 </fieldset>
                 <div class="login__buttons">
                     <a href="../chats/chats.hbs" class="login__submit-button">Войти</a>
