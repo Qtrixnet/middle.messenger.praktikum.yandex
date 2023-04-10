5dfb2ff90da5fcfa1ce80635aa9799e2daea3b87
Kirill <qtrixnet@yandex.ru>
2023-04-10 21:54:45 +0600

    fix: remove hbs setup

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,6 +1,4 @@
 const {JSDOM} = require('jsdom');
-const Handlebars = require('handlebars');
-const fs = require('fs');
 
 const {window} = new JSDOM('<div id="app"></div>', {
     url: 'http://localhost:3000',
@@ -9,12 +7,6 @@ const {window} = new JSDOM('<div id="app"></div>', {
 global.window = window;
 global.document = window.document;
 global.DocumentFragment = window.DocumentFragment;
-
-require.extensions['.hbs'] = function (module, filename) {
-    const contents = fs.readFileSync(filename, 'utf8');
-
-    module.exports = Handlebars.compile(contents);
-}
 
 require.extensions['.pcss'] = function () {
     module.exports = () => ({});
